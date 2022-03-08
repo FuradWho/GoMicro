dd916dc433988fc3be7df25411a6471f9950848b
FuradWho <1337543811@qq.com>
2022-03-08 15:49:10 +0800

    feat: study for design

---
+++ design/adapter/adapter.go
@@ -0,0 +1,55 @@
+package adapter
+
+import "fmt"
+
+/*
+	假设我现在有一个运维系统，需要分别调用阿里云和 AWS 的 SDK 创建主机，
+	两个 SDK 提供的创建主机的接口不一致，此时就可以通过适配器模式，将两
+	个接口统一。 PS：AWS 和 阿里云的接口纯属虚构，没有直接用原始的 SDK，
+	只是举个例子
+*/
+
+// ICreateServer 创建云主机
+type ICreateServer interface {
+	CreateServer(cpu, mem float64) error
+}
+
+// AWSClient aws sdk
+type AWSClient struct{}
+
+// RunInstance 启动实例
+func (c *AWSClient) RunInstance(cpu, mem float64) error {
+	fmt.Printf("aws client run success, cpu： %f, mem: %f", cpu, mem)
+	return nil
+}
+
+// AwsClientAdapter 适配器
+type AwsClientAdapter struct {
+	Client AWSClient
+}
+
+// CreateServer 启动实例
+func (a *AwsClientAdapter) CreateServer(cpu, mem float64) error {
+	a.Client.RunInstance(cpu, mem)
+	return nil
+}
+
+// AliyunClient aliyun sdk
+type AliyunClient struct{}
+
+// CreateServer 启动实例
+func (c *AliyunClient) CreateServer(cpu, mem int) error {
+	fmt.Printf("aws client run success, cpu： %d, mem: %d", cpu, mem)
+	return nil
+}
+
+// AliyunClientAdapter 适配器
+type AliyunClientAdapter struct {
+	Client AliyunClient
+}
+
+// CreateServer 启动实例
+func (a *AliyunClientAdapter) CreateServer(cpu, mem float64) error {
+	a.Client.CreateServer(int(cpu), int(mem))
+	return nil
+}
---
+++ design/factory/simple_factory/simple_factory.go
@@ -0,0 +1,32 @@
+package simple_factory
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+type jsonRuleConfigParser struct {
+}
+
+func (j jsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+// yamlRuleConfigParser yamlRuleConfigParser
+type yamlRuleConfigParser struct {
+}
+
+// Parse Parse
+func (y yamlRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+// NewIRuleConfigParser 1
+func NewIRuleConfigParser(t string) IRuleConfigParser {
+	switch t {
+	case "json":
+		return jsonRuleConfigParser{}
+	case "yaml":
+		return yamlRuleConfigParser{}
+	}
+	return nil
+}
